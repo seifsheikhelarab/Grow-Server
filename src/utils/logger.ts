fbc8718c1aebab9511ba075aa3f74df65aacf544
Seif Sheikhelarab <seifusama@gmail.com>
2025-12-15 13:32:37 +0200

    attempt to fix pino-pretty

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,16 +1,26 @@
 import { pino } from "pino";
+import dotenv from "dotenv";
 
-const logger = pino({
-    level: process.env.LOG_LEVEL || "info",
-    transport: {
-        target: "pino-pretty",
-        options: {
-            colorize: true,
-            singleLine: false,
-            ignore: "pid,hostname"
-        }
-    },
-    timestamp: pino.stdTimeFunctions.isoTime
-});
+dotenv.config({ quiet: true });
+
+const isDev = process.env.NODE_ENV !== "production";
+
+const logger = isDev
+	? pino({
+		level: process.env.LOG_LEVEL || "debug",
+		transport: {
+			target: "pino-pretty",
+			options: {
+				colorize: true,
+				singleLine: false,
+				ignore: "pid,hostname",
+			},
+		},
+		timestamp: pino.stdTimeFunctions.isoTime,
+	})
+	: pino({
+		level: process.env.LOG_LEVEL || "info",
+		timestamp: pino.stdTimeFunctions.isoTime,
+	});
 
 export default logger;
